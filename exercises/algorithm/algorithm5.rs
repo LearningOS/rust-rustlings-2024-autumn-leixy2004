6e0a5b80416a7f67355fb8f5323dbae8abb61947
Anakin Lei <leixy2004@gmail.com>
2024-10-18 01:35:07 +0800

    solve algo 5,6

--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic BFS algorithm
 */
 
-//I AM NOT DONE
 use std::collections::VecDeque;
 
 // Define a graph
@@ -27,10 +26,20 @@ impl Graph {
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
-
+        let mut visited = vec![false;self.adj.len()];
         let mut visit_order = vec![];
+        let mut queue = VecDeque::new();
+        queue.push_back(start);
+        visited[start]=true;
+        while let Some(x) = queue.pop_front() {
+            visit_order.push(x);
+            for &y in &self.adj[x] {
+                if !visited[y] {
+                    visited[y] = true;
+                    queue.push_back(y);
+                }
+            }
+        }
         visit_order
     }
 }
