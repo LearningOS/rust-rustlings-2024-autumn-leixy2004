fe653b321b8f768d3585629dc3eef9f88b0e3e38
Anakin Lei <leixy2004@gmail.com>
2024-10-18 08:56:37 +0800

    solve algo7...8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,8 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+
+use std::fmt::Debug;
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -68,14 +69,22 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        while let Ok(front) = self.q1.dequeue() {
+            self.q2.enqueue(front);
+        }
+        self.q1.enqueue(elem);
+        while let Ok(front) = self.q2.dequeue() {
+            self.q1.enqueue(front);
+        }
+        // println!("{:?}  :  {:?}",self.q1,self.q2);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        self.q1.dequeue().map_err(|_| "Stack is empty")
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
